defc0c80595376618d93a474244db24d8d8c236e
Gaston Casini <gastoncasini@gmail.com>
2020-01-22 18:26:25 -0300

    modified about page copy, fixed minor styling

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -9,13 +9,13 @@ const About = (props) => {
       <div className="section__text-container">
         <h1 className="section__title">About me</h1>
         <p className="section__para">
-          Hi, i'm gaston an argentine <strong>software developer</strong>
-          currently working with <strong>web tecnologies</strong>. I'm allways
-          looking foward to learn the latest tecniques to apply them in my code.
+          Hi, i'm gaston an argentine software developer currently working with
+          <strong> web technologies</strong>.
         </p>
         <p className="section__para">
-          I love to write scalable and efficient solutions to challenging
-          problems. One of my goals is to be part of an exelent workgroup.
+          I love to write <strong>scalable</strong> and{" "}
+          <strong> efficient</strong> solutions to challenging problems. One of
+          my goals is to be part of an exelent workgroup.
           <br /> Im open to job Offers!
         </p>
         <p className="section__para">
