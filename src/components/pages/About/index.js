401e3ca8e2b00184409a46286a737a02256abe93
Gaston Casini <gastoncasini@gmail.com>
2020-01-21 21:50:19 -0300

    final details

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -9,21 +9,17 @@ const About = (props) => {
       <div className="section__text-container">
         <h1 className="section__title">About me</h1>
         <p className="section__para">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-          rem commodi, sed expedita voluptates repudiandae, in quia temporibus
-          voluptatum accusant <strong> voluptates repudiandae, in quia </strong>
-          ium aut harum natus itaque officia! Eos corporis pariatur in nihil.
-        </p>
-
-        <p className="section__para">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-          rem commodi, sed expedita{" "}
-          <strong> voluptates repudiandae, in quia </strong>
-          temporibus
+          Hi, i'm gaston an argentine <strong>software developer</strong>
+          currently working with <strong>web tecnologies</strong>. I'm allways
+          looking foward to learn the latest tecniques to apply them in my code.
         </p>
         <p className="section__para">
-          voluptatum accusantium aut harum natus itaque officia! Eos corporis
-          pariatur in nihil.
+          I love to write scalable and efficient solutions to challenging
+          problems. One of my goals is to be part of an exelent workgroup.
+          <br /> Im open to job Offers!
+        </p>
+        <p className="section__para">
+          Passionate about math, Space and weigthlifting.
         </p>
       </div>
     </Page>
